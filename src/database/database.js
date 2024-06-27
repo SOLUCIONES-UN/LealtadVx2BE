a87d4b24128e2f7251dde81962193d199e2d9269
ansalazar <asalazar@solucionesun.com>
2024-06-27 15:10:59 -0600

    Actualizar rama

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,11 @@
 
 const { Sequelize } = require("sequelize");
 
+// const sequelize = new Sequelize("lealtadv217", "root", "B@rilhas2003", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+
 
 // const sequelize = new Sequelize("lealtadv2", "root", "test", {
 //   host: "localhost",
@@ -8,16 +13,14 @@ const { Sequelize } = require("sequelize");
 //   dialect: "mysql",
 // });
 
-const sequelize = new Sequelize(
-    "dbepco7agwmwba",
-    "uhxm0qdfefvou",
-    "DesaSolUn",
+const sequelize = new Sequelize("dbepco7agwmwba", "devusr","efHBxdcV",
     {
-        host: "34.174.109.166",
-        host: "34.174.109.166",
+        // host: "34.174.109.166",
+        host: "172.16.50.27",
         dialect: "mysql",
     }
 );
+
 
 
 const pronet = new Sequelize("pronet", "devusr", "efHBxdcV", {
--- src/routes/promocion.routes.js
+++ src/routes/promocion.routes.js
@@ -1,22 +1,19 @@
 const {Router} = require('express');
 const router = Router();
-const {GetPromocion,AddPromocion, GetPromocionById, UpdatePromocion, PausarPromocion, ActivarPromocion,TestearCodigo, DeletePromocion,checkNemonico} = require('../controllers/promocion.controller')
+const {GetPromocion,AddPromocion, GetPromocionById, UpdatePromocion, PausarPromocion, ActivarPromocion,TestearCodigo, DeletePromocion,checkNemonico, checkNombre} = require('../controllers/promocion.controller')
 const authUser = require('../middlewares/auth.js');
 const { Promocion } = require('../models/promocion.js');
 
-//declarampos nuestra constante para almacenar el path`
 const path = 'Promocion';
 
-
-//rutas del proyecto
-router.get(`/${path}`, GetPromocion);
+router.get(`/${path}`,authUser, GetPromocion);
 router.get(`/${path}/:id`, GetPromocionById);
 router.post(`/${path}`, AddPromocion);
 router.put(`/${path}/:id`, UpdatePromocion);
 router.put(`/${path}/Pau/:id`,PausarPromocion);
- router.post('/api/check-nemonico', checkNemonico);
+router.post('/api/check-nemonico', checkNemonico);
+router.post(`/${path}/checkNombre`, checkNombre);
 router.put(`/${path}/Act/:id`, ActivarPromocion);
-//router.post(`/${path}/Testear`,TestearCodigo);
 router.delete(`/${path}/:id`, DeletePromocion);
 router.get('/Promocion/exists', async (req, res) => {
     const { nombre } = req.query;
