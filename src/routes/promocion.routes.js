1d017c39d9a6896ebc2eb99de9545aa4a309ebdf
ansalazar <asalazar@solucionesun.com>
2024-06-27 22:25:21 -0600

    funcionalidad de promociones

--- src/routes/promocion.routes.js
+++ src/routes/promocion.routes.js
@@ -15,20 +15,6 @@ router.post('/api/check-nemonico', checkNemonico);
 router.post(`/${path}/checkNombre`, checkNombre);
 router.put(`/${path}/Act/:id`, ActivarPromocion);
 router.delete(`/${path}/:id`, DeletePromocion);
-router.get('/Promocion/exists', async (req, res) => {
-    const { nombre } = req.query;
-    try {
-      const promocion = await Promocion.findOne({ nombre: nombre.trim() });
-      if (promocion) {
-        return res.json({ exists: true });
-      } else {
-        return res.json({ exists: false });
-      }
-    } catch (error) {
-      console.error('Error checking if name exists:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
 
 
 module.exports = router
